4d867b237e494a82c010e95727fbf1e8e1471c23
Cauã Rosa Do Espírito Santo <20241TIIMI0330@cefetes.br>
2026-03-13 08:47:10 -0300

    Aula de button

---
+++ app/src/main/java/com/example/capitulo4a/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.capitulo4a
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.capitulo4a.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            Toast.makeText(this, "Turma do 5 modulo", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
